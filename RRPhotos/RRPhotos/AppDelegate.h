b3f30c2c7e03ea9952cb0762dfe4399a3d159e94
yi-chen1 <853112433@qq.com>
2012-03-26 20:30:22 +0800

    first commit

---
+++ RRPhotos/RRPhotos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RRPhotos
+//
+//  Created by yi chen on 12-3-26.
+//  Copyright (c) 2012年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
